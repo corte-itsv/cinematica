3ede914d0e8468ed4a9bee0aec700e5d03443a0b
Lisandro <l.cabrera406@itsv.edu.ar>
2026-08-05 15:00:24 -0300

    Agrega cinematica.py

--- fisica/cinematica.py
+++ fisica/cinematica.py
@@ -10,7 +10,8 @@ la cantidad de divisiones y el módulo estándar 'math' para evitar dependencias
 """
 
 #importar librerias necesarias y modulo de constantes !
-
+import math
+import fisica.constantes  as constantes
 
 def calcular_lista_tiempo(tiempo_final: float, divisiones: int) -> list[float]:
     """Genera una lista de tiempos desde 0 hasta tiempo_final dividida en N intervalos.
@@ -22,12 +23,25 @@ def calcular_lista_tiempo(tiempo_final: float, divisiones: int) -> list[float]:
     Returns:
         list[float]: Lista con los instantes de tiempo.
     """
+    paso = tiempo_final / divisiones
+    tiempos = []
+    for i in range(0, (divisiones + 1)):
+        tiempos.append(i * paso)
+    
+    return tiempos
+        
+        
 
 
 # --- FUNCIONES DE MRU ---
 
 def calcular_posicion_mru(posicion_inicial: float, velocidad: float, t: float) -> float:
     """Calcula la posición para un MRU en un tiempo t."""
+    posicion_final_mru = posicion_inicial + (velocidad * t)
+    return posicion_final_mru
+    
+    
+    
 
 def mru(
     posicion_inicial: float,
@@ -52,6 +66,34 @@ def mru(
 
     Valida que el tiempo final sea positivo y que la cantidad de divisiones sea mayor a cero
     """
+    if tiempo_final < 0 or divisiones <= 0:
+        return "Error"
+
+    diccionario_mru = {
+        "t": lista_tiempos,
+        "x": [],
+        "v": [],
+        "a": [],
+    }
+    
+    
+    
+    
+    lista_tiempos = calcular_lista_tiempo(tiempo_final, divisiones)
+
+    for t in lista_tiempos:
+        
+        v = velocidad
+        diccionario_mru["v"].append(v)
+
+        x = calcular_posicion_mru(posicion_inicial, v, t)
+        diccionario_mru["x"].append(x)
+
+        a = 0.0
+        diccionario_mru["a"].append(a)
+
+    return diccionario_mru
+    
 
 
 # --- FUNCIONES DE MRUV ---
@@ -63,10 +105,17 @@ def calcular_posicion_mruv(
     t: float,
 ) -> float:
     """Calcula la posición para un MRUV en un tiempo t."""
+    
+    posicion=posicion_inicial+velocidad_inicial*t+0.5*aceleracion*t*t
+    return posicion
+    
+    
 
 
 def calcular_velocidad_mruv(velocidad_inicial: float, aceleracion: float, t: float) -> float:
     """Calcula la velocidad para un MRUV en un tiempo t."""
+    velocidad=velocidad_inicial+aceleracion*t
+    return velocidad
 
 
 def mruv(
@@ -94,7 +143,28 @@ def mruv(
 
     Valida que el tiempo final sea positivo y que la cantidad de divisiones sea mayor a cero
     """
+    if tiempo_final <= 0 or divisiones <= 0:
+        return None
 
+    lista_tiempos = calcular_lista_tiempo(tiempo_final, divisiones)
+    diccionario_mruv = {
+        "t": lista_tiempos,
+        "x": [],
+        "v": [],
+        "a": [],
+    }
+
+    for t in lista_tiempos:
+        a = aceleracion
+        diccionario_mruv["a"].append(a)
+
+        v = calcular_velocidad_mruv(velocidad_inicial, aceleracion, t)
+        diccionario_mruv["v"].append(v)
+
+        x = calcular_posicion_mruv(posicion_inicial, velocidad_inicial, aceleracion, t)
+        diccionario_mruv["x"].append(x)
+
+    return diccionario_mruv
 
 # --- FUNCIONES DE TIRO OBLICUO ---
 
@@ -108,7 +178,10 @@ def obtener_componentes_velocidad(velocidad_inicial: float, angulo: float) -> tu
     Returns:
         tuple: (v0x, v0y) componentes de la velocidad inicial.
     """
-
+    angulo_rad = math.radians(angulo)
+    v0x=velocidad_inicial*math.cos(angulo_rad)
+    v0y=velocidad_inicial*math.sin(angulo_rad)
+    return v0x, v0y
 
 def calcular_tiempo_vuelo(velocidad_inicial_y: float, altura_inicial: float) -> float:
     """Calcula el tiempo de vuelo de un tiro oblicuo hasta que vuelve a y = 0.
@@ -123,6 +196,15 @@ def calcular_tiempo_vuelo(velocidad_inicial_y: float, altura_inicial: float) -> 
     Valida que el discriminante de la funcion cuadrática asociada sea positivo
     (2 Raices Reales Distintas)
     """
+    discriminante = pow(velocidad_inicial_y, 2) + 2 * constantes.G * altura_inicial
+    if discriminante < 0:
+        return None
+
+    t_vuelo = (velocidad_inicial_y + math.sqrt(discriminante)) / constantes.G
+    if t_vuelo <= 0:
+        return None
+
+    return t_vuelo
 
 
 def tiro_oblicuo(
@@ -152,3 +234,45 @@ def tiro_oblicuo(
             - 'ax': Lista de aceleración en el eje X (cero).
             - 'ay': Lista de aceleración en el eje Y (-G).
     """
+
+    if divisiones <= 0:
+        return None
+
+    velocidad_inicial_x, velocidad_inicial_y = obtener_componentes_velocidad(velocidad_inicial, angulo)
+
+    tiempo_final_vuelo = calcular_tiempo_vuelo(velocidad_inicial_y, altura_inicial)
+    if tiempo_final_vuelo is None:
+        return None
+
+    lista_tiempos = calcular_lista_tiempo(tiempo_final_vuelo, divisiones)
+
+    diccionario_tiro_oblicuo = {
+        't': lista_tiempos,
+        'x': [],
+        'y': [],
+        'vx': [],
+        'vy': [],
+        'ax': [],
+        'ay': [],
+    }
+    
+    for t in lista_tiempos:
+        x = velocidad_inicial_x * t
+        diccionario_tiro_oblicuo['x'].append(x)
+        
+        y = altura_inicial + velocidad_inicial_y * t - 0.5 * constantes.G * t * t
+        diccionario_tiro_oblicuo['y'].append(y)
+        
+        vx = velocidad_inicial_x
+        diccionario_tiro_oblicuo['vx'].append(vx)
+        
+        vy = velocidad_inicial_y - constantes.G * t
+        diccionario_tiro_oblicuo['vy'].append(vy)
+        
+        ax = 0.0
+        diccionario_tiro_oblicuo['ax'].append(ax)
+        
+        ay = -1.0 * constantes.G
+        diccionario_tiro_oblicuo['ay'].append(ay)
+        
+    return diccionario_tiro_oblicuo
