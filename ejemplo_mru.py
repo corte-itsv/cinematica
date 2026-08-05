101bdf1ce77fc09e38b3e4977f230f8868d0f7cb
Jeremias Purro <j.purro119@itsv.edu.ar>
2026-08-05 18:39:03 +0000

    Probando ejemplo

--- ejemplo_mru.py
+++ ejemplo_mru.py
@@ -1,1 +1,11 @@
-print("Hola mundo 2")
+
+from fisica.cinematica import mru
+
+resultado = mru(
+    posicion_inicial=0,
+    velocidad=5,
+    tiempo_final=10,
+    divisiones=5,
+)
+
+print(resultado)
