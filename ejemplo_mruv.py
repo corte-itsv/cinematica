684481b3702581b80e049795caf3426eecdce16b
Eitan <e.landenberg543@itsv.edu.ar>
2026-08-05 15:20:35 +0000

    Agrega animar a tiro oblicuo

--- ejemplo_mruv.py
+++ ejemplo_mruv.py
@@ -4,41 +4,9 @@ from fisica.graficos import graficar_mru_mruv
 resultado = fisica.cinematica.mruv(
     posicion_inicial=0.0,
     velocidad_inicial=10.0,
-    aceleracion=2.0,
+    aceleracion=4.0,
     tiempo_final=4.0,
-    divisiones=4,
+    divisiones=40,
 )
 print(resultado)
 graficar_mru_mruv(resultado)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def porno():
-    print('eitan landenberg')
--- ejemplo_tiro.py
+++ ejemplo_tiro.py
@@ -1,5 +1,7 @@
 import fisica
 from fisica.graficos import graficar_tiro_oblicuo
+from fisica.graficos import animar_tiro_oblicuo
 
-resultado = fisica.cinematica.tiro_oblicuo(10, 10, 10, 10)
-graficar_tiro_oblicuo(resultado)
+resultado = fisica.cinematica.tiro_oblicuo(10, 10, 10, 50)
+
+animar_tiro_oblicuo(resultado)
