d3ca4b8a7fca752531410a46bd6e93bee084399e
Jeremias Purro <j.purro119@itsv.edu.ar>
2026-08-05 18:40:05 +0000

    Probando tiro oblicuo

---
+++ ejemplo_tiro.py
@@ -0,0 +1,11 @@
+from fisica.cinematica import tiro_oblicuo
+
+
+resultado = tiro_oblicuo(
+    velocidad_inicial=20,
+    angulo=45,
+    altura_inicial=0,
+    divisiones=10,
+)
+
+print(resultado)
